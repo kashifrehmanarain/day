07732c8c211eccd565167d19e50bd13a760d3f72
Kashif Rehman <kashif.rehman@arpatech.com>
2018-06-13 14:13:53 +0500

    Footer Widget

--- app/Http/ViewComposers/BottomStoresComposer.php
+++ app/Http/ViewComposers/BottomStoresComposer.php
@@ -8,7 +8,7 @@ class BottomStoresComposer
 {
     public function compose(View $view)
     {
-        $top_stores = \App\Models\Stores::where('is_pinned', 1)->orderBy('id', 'desc')->limit(6)->get();
+        $top_stores = \App\Models\Stores::where('is_pinned', 1)->orderBy('id', 'desc')->limit(12)->get();
         $view->with('top_stores', $top_stores);
     }
 }
