82c67811a7e13f5d9b81f4195d4ac114412d427a
Kashif Rehman <kashif.rehman@arpatech.com>
2018-07-12 13:41:37 +0500

    Home Setup

--- resources/views/site/master.blade.php
+++ resources/views/site/master.blade.php
@@ -162,6 +162,18 @@
 </script>
 @endif
 @yield('js-bottom')
+<script type="text/javascript">
+    var vglnk = { key: '87ea49da04e35c7403c14f8c543b6f73' };
+
+    (function(d, t) {
+        var s = d.createElement(t);
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//cdn.viglink.com/api/vglnk.js';
+        var r = d.getElementsByTagName(t)[0];
+        r.parentNode.insertBefore(s, r);
+    }(document, 'script'));
+</script>
 </body>
 
 </html>
